3d0ed99cd8e1efb49b8505a661a00937b9bcb46a
Justin DoBosh <justin.dobosh@spartans.ut.edu>
2015-02-24 21:08:46 -0500

    added awards feature

--- builds/development/js/controllers/checkins.js
+++ builds/development/js/controllers/checkins.js
@@ -35,4 +35,31 @@ $scope.pickRandom = function(){
 	$scope.recordId = checkinsList.$keyAt(whichRecord);
 }//pick random
 
+$scope.showLove = function(myItem){
+	myItem.show = !myItem.show;
+
+	if (myItem.userState == 'expanded') {
+		myItem.userState = '';
+	}else{
+		myItem.userState = 'expanded';
+	};
+}//showLove
+
+$scope.giveLove = function(myItem, myGift){
+	var refLove = new Firebase(FIREBASE_URL + '/users/' + $scope.whichuser + '/meetings/' + $scope.whichmeeting + '/checkins/' + myItem.$id + '/awards');
+	var checkinsObj = $firebase(refLove);
+
+	var myData = {
+		name: myGift,
+		date: Firebase.ServerValue.TIMESTAMP
+	};
+	checkinsObj.$push(myData);
+}//giveLove
+
+$scope.deleteLove = function(checkinId, award){
+	var refLove = new Firebase(FIREBASE_URL + '/users/' + $scope.whichuser + '/meetings/' + $scope.whichmeeting + '/checkins/' + checkinId + '/awards');
+	var record = $firebase(refLove);
+	record.$remove(award);
+}//deleteLove
+
 })//CheckinsController
